8aec6fef189f0f1abe961e2b52b69ead8b6e45e3
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-12 01:06:35 +0200

    Interpolation, html structure & css

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls: ['./hero.component.css'],
 })
 export class HeroComponent {
-
+  public name: string = 'Ironman';
+  public age: number = 45;
 }
