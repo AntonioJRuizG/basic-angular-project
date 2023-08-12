005a7196256e1179a57a405cc7dfa6e32fb2016b
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-12 23:20:40 +0200

    Componentize main page

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPage } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
-  declarations: [MainPage],
+  declarations: [MainPage, ListComponent, FormComponent],
   imports: [CommonModule],
   exports: [MainPage],
 })
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: 'main-page.component.html',
 })
-export class MainPage {}
+export class MainPage {
+  public characters: Character[] = [
+    {
+      name: 'Krillin',
+      power: 500,
+    },
+    {
+      name: 'Goku',
+      power: 10000,
+    },
+  ];
+}
