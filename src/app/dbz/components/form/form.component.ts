5c50e66bddf9da9fd888f737906a3130159d8f70
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-13 00:26:01 +0200

    Add form submit

--- src/app/dbz/components/form/form.component.ts
+++ src/app/dbz/components/form/form.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css'],
 })
-export class FormComponent {}
+export class FormComponent {
+  public character: Character = {
+    name: '',
+    power: 0,
+  };
+
+  emitCharacter(): void {
+    console.log(this.character);
+    (this.character.name = ''), (this.character.power = 0);
+  }
+}
